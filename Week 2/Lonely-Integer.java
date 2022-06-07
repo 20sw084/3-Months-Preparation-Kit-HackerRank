a6e01d6868076bbad0eadbe167372e6c01894ece
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-06-07 22:03:24 +0500

    Add files via upload

---
+++ Week 2/Lonely-Integer.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int lonelyinteger(List<Integer> a) {
+    // Write your code here
+    int lone=0;
+        for(int i=0;i<a.size();i++){
+            int count=0;
+            lone=a.get(i);
+            for(int j=0;j<a.size();j++){
+                if(lone==a.get(j)){
+                    count++;
+                }
+            }
+            if(count==1) return lone;
+        }
+    return 0;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
