756c8a1265a1b1b13aa132cb68281d85e3d9fc0e
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-06-12 10:31:16 +0500

    Update Sales-By-Match.java

--- Week 3/Sales-By-Match.java
+++ Week 3/Sales-By-Match.java
@@ -4,36 +4,31 @@ import java.security.*;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.function.*;
 import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 class Result {
     public static int sockMerchant(int n, List<Integer> ar) {
         int pair=0;
         for(int i=0;i<ar.size();i++){
+            if(ar.get(i)!=0){
+            int check=ar.get(i);
             for(int j=i+1;j<ar.size();j++){
-                if(ar.get(i)==0 && ar.get(j)==0){
-                    i++;
-                    j++;
-                }
-                else if(ar.get(i)==0){
-                    i++;
-                }
-                else if(ar.get(j)==0){
-                    j++;
-                }
-                else if(ar.get(i)==ar.get(j)){
+                if(  ar.get(j)!=0 && check==ar.get(j)){
                     pair++;
                     ar.set(i,0);
                     ar.set(j,0);
                     break;
                 }
-                // else{
-                //     j=0;
-                // }
+            }
             }
         }
         return pair;
     }
+
 }
 
 public class Solution {
@@ -43,14 +38,9 @@ public class Solution {
 
         int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        List<Integer> ar = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            int arItem = Integer.parseInt(arTemp[i]);
-            ar.add(arItem);
-        }
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
 
         int result = Result.sockMerchant(n, ar);
 
