9f1131ec456eb622426c7ffedf87bbf32a4b6f67
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-06-12 12:13:12 +0500

    Add files via upload

---
+++ Week 2/Counting-Valleys.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int countingValleys(int steps, String path) {
+        int level, valley;
+        level = valley = 0;
+        for (int i = 0; i<steps; i++){
+            if (path.charAt(i)=='U'){
+                level++;
+                if (level==0){valley++;}
+                continue;
+            }
+            if (path.charAt(i)=='D'){
+                level--;
+            }
+        }
+                
+        return valley;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
