0d02c26c1e5e2b2b15bbc9d1751b1262f5bedf55
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2022-10-12 12:06:21 +0500

    Linked list collection

---
+++ Week 6/collect.java
@@ -0,0 +1,12 @@
+package collect;
+
+public interface collect {
+public boolean add(Object obj);
+public void clear();
+public boolean contains(Object obj);
+public boolean Isempty();
+public Iterat iterat();
+public boolean remove(Object obj);
+public int size();
+
+}
