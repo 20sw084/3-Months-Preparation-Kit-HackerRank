74182a08fd0dfa5056f019c18511180f48bdeb51
nayab-nisar <134673262+nayab-nisar@users.noreply.github.com>
2023-10-19 19:56:17 +0500

    search_implementation.cpp

---
+++ search_implementation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int low, int high, int target) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target)
+            return mid;
+        if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1; // Element not found
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target;
+    cout << "Enter the element to search: ";
+    cin >> target;
+    int result = binarySearch(arr, 0, n - 1, target);
+    if (result != -1)
+        cout << "Element found at index " << result << endl;
+    else
+        cout << "Element not found" << endl;
+    return 0;
+}
