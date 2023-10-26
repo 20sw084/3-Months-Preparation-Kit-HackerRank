9daf435533754ae912e38ac16c8b2604a82c0208
Krystizz <148135964+Krystizz@users.noreply.github.com>
2023-10-26 14:58:13 +0530

    Update DungeonGame.py

--- LeetCode Problems/DungeonGame.py
+++ LeetCode Problems/DungeonGame.py
@@ -1,33 +1,34 @@
 # LeetCode Problem 174 : Dungeon Game (Link : https://leetcode.com/problems/dungeon-game/)
-
-def calculateMinimumHP(dungeon): # dungeon: List[List[int]]
-
-    i,j,hp,m,n=0,0,0,len(dungeon),len(dungeon[0])
-    steps=[]
-
-    while i!=m-1 or j!=n-1:
-        steps.append(dungeon[i][j])
-        hp-=dungeon[i][j]
-        if i==m-1: # If the Prince is on the last row of the dungeon
-            j+=1
-        elif j==n-1: # If the Prince is on the last column of the dungeon
-            i+=1
-        else:
-            if dungeon[i+1][j]>dungeon[i][j+1]: # Comparing the possible steps to decide the next step
-                i+=1
-            else:
-                j+=1
-
-    hp=hp-dungeon[i][j]+1
-    steps.append(dungeon[i][j])
-    print('Steps involved :',steps[1:hp]) # Prints the steps taken in order
-
-    return hp # Returns the minimum HP
-
-def main():
-
-    dungeon_test = [[7,-8,-9,1],[-10,0,1,-6],[-11,1,-3,-5],[-12,-1,-1,-4],[-13,-3,-2,-20]] # Test matrix for dungeon
-    print('Minimum HP :',calculateMinimumHP(dungeon_test))
-
-if __name__ == "__main__":
-    main()
+class Solution:
+    def calculateMinimumHP(self, dungeon):
+        # Get the size of the dungeon
+        rows = len(dungeon)
+        cols = len(dungeon[0])
+        # Create a 2D list to store the minimum health required
+        x = [[float('-inf')] * cols for _ in range(rows)]
+        
+        def getMinimumHealth(i, j):
+            # If the knight reaches the princess, return the minimum of 0 and the cell's value
+            if i == rows - 1 and j == cols - 1:
+                return min(0, dungeon[i][j])
+            # If the result for this cell is already calculated, return it
+            if x[i][j] != float('-inf'):
+                return x[i][j]
+            # Calculate the minimum health required when moving right and down
+            right_health = float('-inf')
+            down_health = float('-inf')
+            
+            if j + 1 < cols:
+                right_health = getMinimumHealth(i, j + 1)
+            if i + 1 < rows:
+                down_health = getMinimumHealth(i + 1, j)
+            
+            # Calculate the minimum health required to reach the princess from this cell
+            min_health = dungeon[i][j] + max(right_health, down_health)
+            # Store the result in the x table and return it
+            x[i][j] = min(0, min_health)
+            return x[i][j]
+        
+        # Calculate the minimum initial health required to reach the princess from the starting cell
+        min_initial_health = abs(getMinimumHealth(0, 0)) + 1
+        return min_initial_health
